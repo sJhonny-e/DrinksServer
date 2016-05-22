fef5e0af5c1a2fa87f654ce3f9723ab7f1feb019
sJhonny-e <sjhonny@inbox.com>
2016-05-22 11:57:09 +0100

    added DI using Unity

---
+++ DrinksServer/App_Start/IoCRegistrator.cs
@@ -0,0 +1,23 @@
+﻿using DrinksServer.DAL;
+using DrinksServer.Models;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DrinksServer.App_Start
+{
+    internal static class IoCRegistrator
+    {
+        // just a static method to register everything in the app.
+        // TODO: do this more modularly (register dependencies per each module), or better - automatically :)
+        public static void RegisterIoCContainer(HttpConfiguration config)
+        {
+            var container = new UnityContainer();
+            //container.RegisterType<IDefaultRepository, DefaultRepository>(new HierarchicalLifetimeManager());
+            config.DependencyResolver = new UnityResolver(container);
+        }
+    }
+}
---
+++ DrinksServer/App_Start/UnityResolver.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+
+namespace DrinksServer.App_Start
+{
+    // Simply copied from the MS howto http://www.asp.net/web-api/overview/advanced/dependency-injection
+    // TODO: throw this out and replace with a proper implementation, without pokemon exception handling, with logging, etc..
+    public class UnityResolver : IDependencyResolver
+    {
+        protected IUnityContainer container;
+
+        public UnityResolver(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+            this.container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return new List<object>();
+            }
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = container.CreateChildContainer();
+            return new UnityResolver(child);
+        }
+
+        public void Dispose()
+        {
+            container.Dispose();
+        }
+    }
+}
