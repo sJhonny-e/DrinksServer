a1c655084685bdd48e17bb4b16a826c74485b2c6
sJhonny-e <sjhonny@inbox.com>
2016-05-22 15:50:15 +0100

    created request / response classes for drinks

---
+++ Checkout.ApiClient.Net45/ApiServices/Drinks/ResponseModels/DrinkInventoryList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.ApiServices.Drinks.ResponseModels
+{
+    public class DrinkInventoryList
+    {
+        // Currently no count is implemented on the server side
+        public int Count { get; set; }
+        public List<DrinkInventory> Data { get; set; }
+    }
+}
