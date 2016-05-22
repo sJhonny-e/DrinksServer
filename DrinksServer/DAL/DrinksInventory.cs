9347a5fe5fa82c8c9975350bbae009fe02a5b363
sJhonny-e <sjhonny@inbox.com>
2016-05-22 12:05:28 +0100

    added drinks repository interface

---
+++ DrinksServer/DAL/DrinksInventory.cs
@@ -0,0 +1,21 @@
+﻿using DrinksServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrinksServer.DAL
+{
+    public class DrinksInventory : IDrinksRepository
+    {
+        public DrinkInventory Get(string drinkName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DrinkInventory> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DrinksServer/Models/IDrinksRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksServer.Models
+{
+    public interface IDrinksRepository
+    {
+        IEnumerable<DrinkInventory> GetAll();
+        DrinkInventory Get(string drinkName);
+    }
+}
