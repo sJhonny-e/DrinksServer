8ba5555238c82e3c2dd1b41a501a62f7d146b9ab
sJhonny-e <sjhonny@inbox.com>
2016-05-22 15:51:43 +0100

    created a test console app

---
+++ ClientTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Checkout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiClient = new APIClient("not really a key", Checkout.Helpers.Environment.Sandbox, true);
+            var drinksService = apiClient.DrinksService.GetDrinksList();
+        }
+    }
+}
