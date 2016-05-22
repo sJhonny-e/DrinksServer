48bf051d2f4ab28dc5527a0de0048fe8fd6a439d
sJhonny-e <sjhonny@inbox.com>
2016-05-22 12:08:27 +0100

    added a scaffolded drinks controller

---
+++ DrinksServer/Controllers/DrinksController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DrinksServer.Controllers
+{
+    public class DrinksController : ApiController
+    {
+        // GET: api/Drinks
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Drinks/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Drinks
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Drinks/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Drinks/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
