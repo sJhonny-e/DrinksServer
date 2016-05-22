05747d01e1e717222a875ef1cbe7e2800707fc91
sJhonny-e <sjhonny@inbox.com>
2016-05-22 14:28:31 +0100

    added some unit tests to repository

---
+++ DrinksServer.Tests/DAL/DrinksRepositoryTests.cs
@@ -0,0 +1,48 @@
+﻿using DrinksServer.DAL;
+using DrinksServer.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksServer.Tests.DAL
+{
+    [TestFixture()]
+    public class DrinksRepositoryTests
+    {
+        private DrinksRepository _repository;
+        private DrinkInventory bosco = new DrinkInventory(new Drink("bosco"), 4);
+
+        [SetUp]
+        public void Setup()
+        {
+            _repository = new DrinksRepository();
+        }
+
+        [Test]
+        public void Add_WhenDrinkDoesntExistAlready_AddsTheDrink()
+        {
+            Assert.IsTrue(_repository.Add(bosco));
+
+            Assert.AreEqual(bosco, _repository.Get("bosco"));
+        }
+
+        [Test]
+        public void Add_WhenTryingToAddAnIdenticalDrink_Fails()
+        {
+            Assert.IsTrue(_repository.Add(bosco));
+            var otherBosco = new DrinkInventory(new Drink("boSCo"), 64);
+
+            Assert.IsFalse(_repository.Add(otherBosco));
+        }
+
+        [Test]
+        public void Get_IsCaseInsensitive()
+        {
+            _repository.Add(bosco);
+            Assert.AreEqual(bosco, _repository.Get("BosCO"));
+        }
+    }
+}
