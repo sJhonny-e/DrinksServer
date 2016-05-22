0b8af5ab558740f7bb87b32e24121e2f1fa5c22d
sJhonny-e <sjhonny@inbox.com>
2016-05-22 15:50:40 +0100

    created the drinks service

---
+++ Checkout.ApiClient.Net45/ApiServices/Drinks/DrinksService.cs
@@ -0,0 +1,22 @@
+﻿using Checkout.ApiServices.Drinks.ResponseModels;
+using Checkout.ApiServices.SharedModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.ApiServices.Drinks
+{
+    public class DrinksService
+    {
+        public HttpResponse<DrinkInventoryList> GetDrinksList()
+        {
+            var getDrinksListUri = ApiUrls.Drinks;
+
+            var kuku = new ApiHttpClient().GetRequest<List<DrinkInventory>>(getDrinksListUri, AppSettings.SecretKey);
+
+            return null;
+        }
+    }
+}
