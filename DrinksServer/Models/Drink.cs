442571914fd1e9135fd6f59fe6b4f7a6d0dabc4b
sJhonny-e <sjhonny@inbox.com>
2016-05-22 12:02:18 +0100

    added model classes

---
+++ DrinksServer/Models/Drink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrinksServer.Models
+{
+    public class Drink
+    {
+        public string Name { get; private set; }
+
+        public Drink(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ DrinksServer/Models/DrinkInventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrinksServer.Models
+{
+    public class DrinkInventory
+    {
+        public Drink Drink { get; private set; }
+        public int Quantity { get; private set; }
+
+        public DrinkInventory(Drink drink)
+        {
+            Drink = drink;
+            Quantity = 0;
+        }
+
+        public DrinkInventory(Drink drink, int quantity): this(drink)
+        {
+            Quantity = quantity;
+        }
+    }
+}
