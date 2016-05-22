97b12927f32ed5abcaa7c79525e830cca536eee9
sJhonny-e <sjhonny@inbox.com>
2016-05-22 12:13:27 +0100

    added DrinkDTO class

---
+++ DrinksServer/Controllers/DTOs/DrinkDTO.cs
@@ -0,0 +1,20 @@
+﻿using DrinksServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrinksServer.Controllers.DTOs
+{
+    public class DrinkDTO
+    {
+        public string DrinkName { get; set; }
+        public int Quantity { get; set; }
+
+        public DrinkDTO(DrinkInventory drinkInventory)
+        {
+            DrinkName = drinkInventory.DrinkName;
+            Quantity = drinkInventory.Quantity;
+        }
+    }
+}
